bc6e4aa6f4b7c4dcaeecad318f520d30c96e6dde
Alisher <Alisher.email@example.com>
2025-03-21 14:20:04 +0300

    finish

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,17 +11,20 @@ const AboutMe = () => {
         </h1>
         <h1 className="text-[#000000] text-[17px] mb-4">
           I'm interested in Creating new Social Media websites and everything
+
           connected with it.
         </h1>
         <h1 id="aboutme_text_w" className="text-[#000000] text-[17px] mb-4 w-[40%] text-center mx-auto">
           I'm Ready to implement excellent projects with wonderful people.
         </h1>
         <h1 className="text-black font-bold text-2xl mb-4">Edication</h1>
+
         <h1 className="text-[#000000] text-[17px]">
           As front-end developer I studied in Studying center called "IT Center"
           in
         </h1>
         <h1 className="text-[#000000] text-[17px] mb-6">
+          
           And to develop this, I learned from YouTube, specifically from
           <a href="https://www.youtube.com/@akhrorweb" className="text-blue-700 underline">Akror web</a>.
         </h1>
